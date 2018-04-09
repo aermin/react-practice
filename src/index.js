568656bc0b9e718e4ff0785c9a8cd172947ebfec
aermin <aermin@foxmail.com>
2018-04-09 12:22:13 +0800

    feat:文件路径优化

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {AppContainer} from 'react-hot-loader';
 
-import getRouter from './router/router';
+import getRouter from 'router/router';
 
 /* 初始化 */
 renderWithHotReload(getRouter());
 
 /* 热更新 */
 if (module.hot) {
-    module.hot.accept('./router/router', () => {
-        const getRouter = require('./router/router').default;
+    module.hot.accept('router/router', () => {
+        const getRouter = require('router/router').default;
         renderWithHotReload(getRouter());
     });
 }
